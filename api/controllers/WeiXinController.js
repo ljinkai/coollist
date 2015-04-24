c3e9858f9be7797c4f43b632111bcdaa32e5fc26
ljinkai <ljinkai@126.com>
2015-04-24 16:27:14 +0800

    add body

--- api/controllers/WeiXinController.js
+++ api/controllers/WeiXinController.js
@@ -138,7 +138,7 @@ module.exports = {
         }
     },
     receive: function(req,res) {
-            console.log(JSON.stringify(req.headers) + " print::receive" + req.param("ToUserName"));
+            console.log(JSON.stringify(req.headers) + " print::receive" + req.body);
             // loop
             weixin.loop(req, res);
 
